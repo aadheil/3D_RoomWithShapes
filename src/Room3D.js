02ead4a29896f27549808de4bf83d484436de28f
aadheil <adilkkinbox@gmail.com>
2024-03-05 21:56:10 +0530

    Sphere can move along x and z axis

--- src/Room3D.js
+++ src/Room3D.js
@@ -4,11 +4,16 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei'; // Import OrbitControls
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 import roomglb from './temp-room2.glb'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function Room3D() {
   function Model() {
     const gltf = useGLTF(roomglb); // Replace with the actual path to your GLB model
     return <primitive object={gltf.scene} />;
   }
+  // const notify = () => toast.info("This is a toast notification !");
 
   const [isBoxshow, setisBoxshow] = useState(false)
   const [isSphereShow, setisSphereShow] = useState(false)
@@ -28,6 +33,17 @@ function Room3D() {
   var roomMinwidthZ = -2
 
   const handleBoxShow = () => {
+    var newCubeX=-2
+    var newCubeY=1
+    var newCubeZ=sphereZ+2
+   if(newCubeZ>=roomMaxwidthX){
+    newCubeZ=sphereZ-2
+   }
+   else{
+    newCubeZ=sphereZ+2
+   }
+
+   setboxdir([newCubeX,newCubeY,newCubeZ])
     setisBoxshow(!isBoxshow)
 
   }
@@ -61,11 +77,11 @@ function Room3D() {
   //  increse Box X direction by 1
   const handleBoxXdir = () => {
     if(isSphereShow){
-      if ((boxnewZdir == sphereZ || boxnewZdir == (sphereZ - 1) || boxnewZdir == (sphereZ + 1)) && ((boxXnewDirX + 1) == -2 || (boxXnewDirX + 1) == -1 || (boxXnewDirX + 1) == -3)) {
-        alert("Action Declined Due to Overlap")
+      if ((boxnewZdir == sphereZ || boxnewZdir == (sphereZ - 1) || boxnewZdir == (sphereZ + 1)) && ((boxXnewDirX + 1) == (sphereX+1) || (boxXnewDirX + 1) == (sphereX-1) || (boxXnewDirX + 1) == sphereX)) {
+        toast.warning("Action Declined Due To Overlap")
       }
       else if ((boxXnewDirX + 1) > roomMaxwidthX) {
-        alert("Action Declined Due to Exceeding Base Coordinations")
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")
       }
       else {
         boxXnewDirX = boxdir[0] + 1;
@@ -73,7 +89,7 @@ function Room3D() {
       }
     }
     else if ((boxXnewDirX + 1) > roomMaxwidthX) {
-      alert("Action Declined Due to Exceeding Base Coordinations")
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")
     }
     else {
       boxXnewDirX = boxdir[0] + 1
@@ -85,13 +101,12 @@ function Room3D() {
   const handleBoxXdirReduce = () => {
     if(isSphereShow){
       if (
-        (boxnewZdir === sphereZ || boxnewZdir === sphereZ - 1 || boxnewZdir === sphereZ + 1) &&
-        ((boxXnewDirX - 1) === -2 || (boxXnewDirX - 1) === -1 || (boxXnewDirX - 1) === -3)) {
-        alert('Action Declined Due to Overlap');
-      }
+        (boxnewZdir === sphereZ || boxnewZdir === (sphereZ - 1) || boxnewZdir === (sphereZ + 1)) &&
+        ((boxXnewDirX - 1) === (sphereX+1) || (boxXnewDirX - 1) === (sphereX-1) || (boxXnewDirX - 1) === sphereX)) {
+          toast.warning("Action Declined Due To Overlap")
+        }
       else if ((boxXnewDirX - 1) < roomMinwidthX) {
-        alert("Action Declined Due to Exceeding Base Coordinations")
-      }
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")      }
       else {
         boxXnewDirX = boxdir[0] - 1;
         setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
@@ -99,35 +114,30 @@ function Room3D() {
       
     }
     else if ((boxXnewDirX - 1) < roomMinwidthX) {
-      alert("Action Declined Due to Exceeding Base Coordinations")
-    }
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")    }
     else {
       boxXnewDirX = boxdir[0] - 1;
       setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
     }
   };
 
-
   //  increase Box Z direction by 1
-
   const handleBoxZdirInc = () => {
     if(isSphereShow){
       if (
-        ((boxnewZdir + 1) === sphereZ || (boxnewZdir + 1) === sphereZ - 1 || (boxnewZdir + 1) === sphereZ + 1) &&
-        (boxXnewDirX === -2 || boxXnewDirX === -1 || boxXnewDirX === -3)) {
-        alert('Action Declined Due to Overlap');
-      }
+        ((boxnewZdir + 1) === sphereZ || (boxnewZdir + 1) === (sphereZ - 1) || (boxnewZdir + 1) === (sphereZ + 1)) &&
+        (boxXnewDirX === (sphereX+1) || boxXnewDirX === (sphereX-1) || boxXnewDirX === sphereX)) {
+          toast.warning("Action Declined Due To Overlap")
+        }
       else if ((boxnewZdir + 1) > roomMaxwidthZ) {
-        alert("Action Declined Due to Exceeding Base Coordinations")
-      }
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")      }
       else {
         boxnewZdir = boxdir[2] + 1;
         setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
       }
     }
     else if ((boxnewZdir + 1) > roomMaxwidthZ) {
-      alert("Action Declined Due to Exceeding Base Coordinations")
-    }
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")    }
     else {
       boxnewZdir = boxdir[2] + 1;
       setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
@@ -138,30 +148,116 @@ function Room3D() {
   const handleBoxZdirDec = () => {
    if(isSphereShow){
     if (
-      ((boxnewZdir - 1) === sphereZ || (boxnewZdir - 1) === sphereZ - 1 || (boxnewZdir - 1) === sphereZ + 1) &&
-      (boxXnewDirX === -2 || boxXnewDirX === -1 || boxXnewDirX === -3)) {
-      alert('Action Declined Due to Overlap');
+      ((boxnewZdir - 1) === sphereZ || (boxnewZdir - 1) === (sphereZ - 1) || (boxnewZdir - 1) === (sphereZ + 1)) &&
+      (boxXnewDirX === (sphereX+1) || boxXnewDirX === (sphereX-1) || boxXnewDirX === sphereX)) {
+        toast.warning("Action Declined Due To Overlap")
     }
     else if ((boxnewZdir - 1) < roomMinwidthZ) {
-      alert("Action Declined Due to Exceeding Base Coordinations")
-    }
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")    }
     else {
       boxnewZdir = boxdir[2] - 1;
       setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
     }
    }
    else if ((boxnewZdir - 1) < roomMinwidthZ) {
-    alert("Action Declined Due to Exceeding Base Coordinations")
-  }
+    toast.warning("Action Declined Due to Exceeding Base Coordinations")  }
     else {
       boxnewZdir = boxdir[2] - 1;
       setboxdir([boxXnewDirX, boxnewYdir, boxnewZdir]);
     }
   };
 
+  //  increse Sphere X direction by 1
+  const handleSphereXdirInc = () => {
+    if(isBoxshow){
+      if (((boxnewZdir+1) == sphereZ || (boxnewZdir-1) == sphereZ  || boxnewZdir == sphereZ) && ((boxXnewDirX + 1) == (sphereX+1) || (boxXnewDirX - 1) == (sphereX+1) || boxXnewDirX == (sphereX+1))) {
+        toast.warning("Action Declined Due To Overlap")
+      }
+      else if ((sphereX + 1) > roomMaxwidthX) {
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")      }
+      else {
+        sphereX = sphereDir[0] + 1;
+        setsphereDir([sphereX, 1, sphereZ]);
+      }
+    }
+    else if ((sphereX + 1) > roomMaxwidthX) {
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")    }
+    else {
+      sphereX = sphereDir[0] + 1;
+      setsphereDir([sphereX, 1, sphereZ]);
+    }
+  }
+
+  //  Decrease Sphere X direction by 1
+  const handleSphereXdirDec = () => {
+    if(isBoxshow){
+      if (((boxnewZdir+1) == sphereZ || (boxnewZdir-1) == sphereZ  || boxnewZdir == sphereZ) && ((boxXnewDirX + 1) == (sphereX-1) || (boxXnewDirX - 1) == (sphereX-1) || boxXnewDirX == (sphereX-1))) {
+        toast.warning("Action Declined Due To Overlap")
+      }
+      else if ((sphereX - 1) < roomMinwidthX) {
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")      }
+      else {
+        sphereX = sphereDir[0] - 1;
+        setsphereDir([sphereX, 1, sphereZ]);
+      }
+    }
+    else if ((sphereX - 1) < roomMinwidthX) {
+      toast.warning("Action Declined Due to Exceeding Base Coordinations")    }
+    else {
+      sphereX = sphereDir[0] - 1;
+      setsphereDir([sphereX, 1, sphereZ]);
+    }
+  }
+
+  //  increase Sphere Z direction by 1
+    const handleSphereZdirInc = () => {
+      if(isBoxshow){
+        if (
+          ((boxnewZdir + 1) === (sphereZ+1) || (boxnewZdir - 1) === (sphereZ+1) || boxnewZdir === (sphereZ+1)) &&
+          ((boxXnewDirX+1) === sphereX || (boxXnewDirX-1) === sphereX || boxXnewDirX === sphereX)) {
+            toast.warning("Action Declined Due To Overlap")
+          }
+        else if ((sphereZ + 1) > roomMaxwidthZ) {
+          toast.warning("Action Declined Due to Exceeding Base Coordinations")        }
+        else {
+          sphereZ = sphereDir[2] + 1;
+          setsphereDir([sphereX, 1, sphereZ]);
+        }
+      }
+      else if ((sphereZ + 1) > roomMaxwidthZ) {
+        toast.warning("Action Declined Due to Exceeding Base Coordinations")      }
+      else {
+        sphereZ = sphereDir[2] + 1;
+          setsphereDir([sphereX, 1, sphereZ]);
+      }
+    };
+
+      //  decrease Sphere Z direction by 1
+      const handleSphereZdirDec = () => {
+        if(isBoxshow){
+          if (
+            ((boxnewZdir + 1) === (sphereZ-1) || (boxnewZdir - 1) === (sphereZ-1) || boxnewZdir === (sphereZ-1)) &&
+            ((boxXnewDirX+1) === sphereX || (boxXnewDirX-1) === sphereX || boxXnewDirX === sphereX)) {
+              toast.warning("Action Declined Due To Overlap")
+            }
+          else if ((sphereZ - 1) < roomMinwidthZ) {
+            toast.warning("Action Declined Due to Exceeding Base Coordinations")          }
+          else {
+            sphereZ = sphereDir[2] - 1;
+            setsphereDir([sphereX, 1, sphereZ]);
+          }
+        }
+        else if ((sphereZ - 1) < roomMinwidthZ) {
+          toast.warning("Action Declined Due to Exceeding Base Coordinations")        }
+        else {
+          sphereZ = sphereDir[2] - 1;
+            setsphereDir([sphereX, 1, sphereZ]);
+        }
+      };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '100%' }}>
+      <ToastContainer autoClose={1500}/>
       <Canvas style={{ height: '80vh', width: '100%', background: 'lightblue' }}>
         <OrbitControls /> {/* Add OrbitControls to enable camera navigation */}
         {/* Load GLTF Model */}
@@ -191,6 +287,8 @@ function Room3D() {
 
       </Canvas>
       <div style={{ display: 'flex', flexDirection: 'row', marginTop: '10px', height: '40px', width: '100%', justifyContent: 'center', flexWrap: 'wrap' }}>
+
+        {/* cube */}
         <div style={{ display: 'flex', flexDirection: 'column', width: '20%' }}>
           <div>
             <button onClick={handleBoxShow} style={{ width: '100%', height: '40px' }}>{!isBoxshow ? 'Add Cube' : 'Remove Cube'}</button>
@@ -207,10 +305,29 @@ function Room3D() {
           </div>}
 
         </div>
+         
+         {/* sphere */}
+         <div style={{ display: 'flex', flexDirection: 'column', width: '20%',marginLeft:'20px' }}>
+          <div>
+          <button onClick={handleSphereShow} style={{  width: '100%', height: '40px' }}>{!isSphereShow ? 'Add Sphere' : 'Remove Sphere'}</button>
+          </div>
 
-        <div style={{ display: 'flex', flexDirection: 'column', width: '20%' }}>
-          <button onClick={handleSphereShow} style={{ marginLeft: '20px', width: '100%', height: '40px' }}>{!isSphereShow ? 'Add Sphere' : 'Remove Sphere'}</button>
+          {isSphereShow && <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '15px' }}>
+            <button style={{ width: '70px', height: '25px' }} onClick={handleSphereXdirDec}><i className="fa-solid fa-arrow-left fa-lg"></i></button>
+            <button style={{ width: '70px', height: '25px' }} onClick={handleSphereXdirInc}><i className="fa-solid fa-arrow-right fa-lg"></i></button>
+          </div>
+          }
+          {isSphereShow && <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '15px' }}>
+            <button style={{ width: '70px', height: '25px' }} onClick={handleSphereZdirInc}><i className="fa-solid fa-arrow-down fa-lg"></i></button>
+            <button style={{ width: '70px', height: '25px' }} onClick={handleSphereZdirDec}> <i className="fa-solid fa-arrow-up fa-lg"></i></button>
+          </div>}
+
         </div>
+
+
+        
+
+
 
         <div style={{ display: 'flex', flexDirection: 'column', width: '20%' }}>
           <button style={{ marginLeft: '40px', width: '100%', height: '40px' }} onClick={handlePlaneShapeShow}>{!isPlaneShapeShow ? 'Add Plane' : 'Remove Plane'}</button>
